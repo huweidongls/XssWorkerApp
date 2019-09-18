47ccc6c9613413eb26ce6784f2a1fe65749b548d
huweidong <466463054@qq.com>
2019-09-18 16:27:05 +0800

    123123

--- app/src/main/java/com/jingna/xssworkerapp/fragment/FragmentMessage.java
+++ app/src/main/java/com/jingna/xssworkerapp/fragment/FragmentMessage.java
@@ -1,6 +1,8 @@
 package com.jingna.xssworkerapp.fragment;
 
+import android.content.Context;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
@@ -15,6 +17,11 @@ import com.jingna.xssworkerapp.base.BaseFragment;
 import com.jingna.xssworkerapp.bean.WorkerMessageListBean;
 import com.jingna.xssworkerapp.net.NetUrl;
 import com.jingna.xssworkerapp.util.SpUtils;
+import com.scwang.smartrefresh.header.MaterialHeader;
+import com.scwang.smartrefresh.layout.SmartRefreshLayout;
+import com.scwang.smartrefresh.layout.api.RefreshLayout;
+import com.scwang.smartrefresh.layout.listener.OnRefreshListener;
+import com.scwang.smartrefresh.layout.listener.OnRefreshLoadMoreListener;
 import com.vise.xsnow.http.ViseHttp;
 import com.vise.xsnow.http.callback.ACallback;
 
@@ -35,6 +42,8 @@ public class FragmentMessage extends BaseFragment {
 
     @BindView(R.id.rv)
     RecyclerView recyclerView;
+    @BindView(R.id.refresh)
+    SmartRefreshLayout smartRefreshLayout;
 
     private MessageAdapter adapter;
     private List<WorkerMessageListBean.ObjBean> mList;
@@ -51,6 +60,42 @@ public class FragmentMessage extends BaseFragment {
     }
 
     private void initData() {
+
+        smartRefreshLayout.setEnableLoadMore(false);
+        smartRefreshLayout.setRefreshHeader(new MaterialHeader(getContext()));
+        smartRefreshLayout.setOnRefreshListener(new OnRefreshListener() {
+            @Override
+            public void onRefresh(@NonNull final RefreshLayout refreshLayout) {
+                ViseHttp.POST(NetUrl.worker_message_listUrl)
+                        .addParam("uid", SpUtils.getUid(getContext()))
+                        .request(new ACallback<String>() {
+                            @Override
+                            public void onSuccess(String data) {
+                                try {
+                                    JSONObject jsonObject = new JSONObject(data);
+                                    if(jsonObject.optInt("code") == 200){
+                                        Gson gson = new Gson();
+                                        WorkerMessageListBean bean = gson.fromJson(data, WorkerMessageListBean.class);
+                                        mList = bean.getObj();
+                                        adapter = new MessageAdapter(mList);
+                                        LinearLayoutManager manager = new LinearLayoutManager(getContext());
+                                        manager.setOrientation(LinearLayoutManager.VERTICAL);
+                                        recyclerView.setLayoutManager(manager);
+                                        recyclerView.setAdapter(adapter);
+                                    }
+                                    refreshLayout.finishRefresh(500);
+                                } catch (JSONException e) {
+                                    e.printStackTrace();
+                                }
+                            }
+
+                            @Override
+                            public void onFail(int errCode, String errMsg) {
+                                refreshLayout.finishRefresh(500);
+                            }
+                        });
+            }
+        });
 
         ViseHttp.POST(NetUrl.worker_message_listUrl)
                 .addParam("uid", SpUtils.getUid(getContext()))
